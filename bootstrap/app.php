95015cc6dcd250157a92a2ccfd863de5f880c786
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-02-23 22:44:27 +0530

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'rolemanager'->RoleManager::class
+            'rolemanager' => RoleManager::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
