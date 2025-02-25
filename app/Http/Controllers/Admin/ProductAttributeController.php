3cc18915dddccb3ebd3486c0b5104f875ab14553
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-02-25 13:32:46 +0530

    Update ProductAttributeController.php

--- app/Http/Controllers/Admin/ProductAttributeController.php
+++ app/Http/Controllers/Admin/ProductAttributeController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class ProductAttributeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.product_attribute.create');
+    }
+
+    public function manage()
+    {
+        return view('admin.product_attribute.manage');
+    }
 }
