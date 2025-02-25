059fbec2f8fcfda68ac33d911f91ab3a80593f3a
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-02-25 13:32:37 +0530

    Update CategoryController.php

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.category.create');
+    }
+
+    public function manage()
+    {
+        return view('admin.category.manage');
+    }
 }
