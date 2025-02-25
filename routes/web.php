6cb72afff349c62e4a635f658a8d448def83528e
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-02-25 11:42:38 +0530

    route updated for latest controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminMainController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\ProductAttributeController;
+use App\Http\Controllers\Admin\ProductDiscountController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +28,31 @@ Route::middleware(['auth', 'verified','rolemanager:admin'])->group(function () {
             Route::get('/manage/stores', 'stores')->name('admin.manage.store');
             Route::get('/cart/history', 'cart_history')->name('admin.cart.history');
             Route::get('/order/history', 'order_history')->name('admin.order.history');
+        });
 
+        Route::controller(CategoryController::class)->group(function () {
+            Route::get('/category/create', 'index')->name('category.create');
+            Route::get('/category/manage', 'manage')->name('category.manage');
+        });
+
+        Route::controller(SubCategoryController::class)->group(function () {
+            Route::get('/subcategory/create', 'index')->name('subcategory.create');
+            Route::get('/subcategory/manage', 'manage')->name('subcategory.manage');
+        });
+
+        Route::controller(ProductController::class)->group(function () {
+            Route::get('/product/manage', 'index')->name('product.manage');
+            Route::get('/product/review/manage', 'manage')->name('product.review.manage');
+        });
+
+        Route::controller(ProductAttributeController::class)->group(function () {
+            Route::get('/productattribute/create', 'index')->name('productattribute.create');
+            Route::get('/productattribute/manage', 'manage')->name('productattribute.manage');
+        });
+
+        Route::controller(ProductDiscountController ::class)->group(function () {
+            Route::get('/discount/create', 'index')->name('discount.create');
+            Route::get('/discount/manage', 'manage')->name('discount.manage');
         });
     });
 });
