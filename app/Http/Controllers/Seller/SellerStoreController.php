643bd9d7feff3005db8887ac79109449afce5702
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-02-26 22:18:16 +0530

    Update SellerStoreController.php

--- app/Http/Controllers/Seller/SellerStoreController.php
+++ app/Http/Controllers/Seller/SellerStoreController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class SellerStoreController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('seller.store.create');
+    }
+
+    public function manage()
+    {
+        return view('seller.store.manage');
+    }
 }
