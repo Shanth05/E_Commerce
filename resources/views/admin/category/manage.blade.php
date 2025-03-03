3972d7821187b4e3ce1d3a6c0ea57ee5ee6b6e77
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-03 23:17:51 +0530

    Update manage.blade.php

--- resources/views/admin/category/manage.blade.php
+++ resources/views/admin/category/manage.blade.php
@@ -1,7 +1,37 @@
 @extends('admin.layouts.layout')
 @section('admin_page_title')
-Manage Category - Admin Panel
+    Manage Category
 @endsection
 @section('admin_layout')
-    <h3>Manage Category</h3>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title mb-0">All Category</h5>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Category Name</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($categories as $cat)
+                                    <tr>
+                                        <td>{{ $cat->id }}</td>
+                                        <td>{{ $cat->category_name }}</td>
+                                        <td><a href="#" class="btn btn-danger">Delete</a></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
