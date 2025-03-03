1049f26f3d696f92a77d72f50f3a61a06efd81d4
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-03 20:50:30 +0530

    Update MasterCategoryController.php

--- app/Http/Controllers/MasterCategoryController.php
+++ app/Http/Controllers/MasterCategoryController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class MasterCategoryController extends Controller
 {
-    //
+    public function storecat(Request $request):void{
+        $validate_data = $request->validate([
+            'category_name' => 'unique:categories|max:100',
+        ]);
+
+        Category::create($validate_data);
+    }
 }
