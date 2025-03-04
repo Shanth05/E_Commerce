b02f15f954f8e40ad47a573b5718e27925285486
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-04 13:58:10 +0530

    create category_info function

--- app/Http/Controllers/MasterCategoryController.php
+++ app/Http/Controllers/MasterCategoryController.php
@@ -9,11 +9,16 @@ class MasterCategoryController extends Controller
 {
     public function storecat(Request $request){
         $validate_data = $request->validate([
-            'category_name' => 'unique:categories|max:100|min:10',
+            'category_name' => 'unique:categories|max:100|min:5',
         ]);
 
         Category::create($validate_data);
 
-        return redirect()->back();
+        return redirect()->back()->with('success', 'Category Added Successfully');
+    }
+
+    public function show($id){
+        $category_info = Category::find($id);
+        return view('admin.category.edit', compact('category_infoegory'));
     }
 }
