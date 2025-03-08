2e75c99baa0ca587d4de45443bbb62dcc3db0ec2
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-08 15:28:51 +0530

    Update MasterSubCategoryController.php

--- app/Http/Controllers/MasterSubCategoryController.php
+++ app/Http/Controllers/MasterSubCategoryController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class MasterSubCategoryController extends Controller
 {
-    //
+    public function storecat(Request $request)
+    {
+       $validate_data = $request->validate([
+        'subcategory_name' => 'unique:sub_categories|max:100|min:5',
+        'category_id' => 'required|exists:categories,id',
+       ]);
+
+       Subcategory::create($validate_data);
+    
+       return redirect()->back()->with('message', 'Sub Category Added Successfully');
+    }
 }
