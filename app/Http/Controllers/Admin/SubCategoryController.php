a4497999d679a500579e76919c2cbbd5523b8d4b
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-09 00:56:30 +0530

    Update SubCategoryController.php

--- app/Http/Controllers/Admin/SubCategoryController.php
+++ app/Http/Controllers/Admin/SubCategoryController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class SubCategoryController extends Controller
 {
     public function index()
     {
-        $catagories = Category::all();
-        return view('admin.sub_category.create', compact('catagories'));
+        $categories = Category::all();
+        return view('admin.sub_category.create', compact('categories'));
     }
 
     public function manage()
