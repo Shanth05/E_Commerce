a99ff94a0423ddc23c057e10ab19aa0a672efd85
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-11 00:15:12 +0530

    Update create.blade.php

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -28,7 +28,7 @@
                     <form action="{{ route('store.cat') }}" method="POST">
                         @csrf
                         <label for="category_name" class="fw-bold mb-2">Give Name of Your Category</label>
-                        <input type="text" class="form-control" name="category_name" placeholder="Computer" required>
+                        <input type="text" class="form-control" name="category_name" placeholder="Computer">
 
                         <button type="submit" class="btn btn-primary w-100 mt-2">Add Category</button>
                     </form>
