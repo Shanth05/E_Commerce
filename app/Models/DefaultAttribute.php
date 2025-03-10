43f3ebb0e76d3fb00f27bbb89f74edd54eedf370
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-11 00:15:09 +0530

    Update DefaultAttribute.php

--- app/Models/DefaultAttribute.php
+++ app/Models/DefaultAttribute.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DefaultAttribute extends Model
 {
-    //
+    use HasFactory;
+
+    use HasFactory;
+
+    protected $fillable = [
+        'attribute_value',
+        
+    ];
 }
