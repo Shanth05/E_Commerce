04ed77f033a24832e09fb0aff6d4f7105462e0df
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-13 09:50:34 +0530

    created store function

--- app/Http/Controllers/Seller/SellerStoreController.php
+++ app/Http/Controllers/Seller/SellerStoreController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
+use App\Models\Store;
 use Illuminate\Http\Request;
 
 class SellerStoreController extends Controller
@@ -15,5 +16,23 @@ class SellerStoreController extends Controller
     public function manage()
     {
         return view('seller.store.manage');
+    }
+
+    public function store(Request $request)
+    {
+        $validate_data = $request->validate([
+            'store_name' => 'unique:stores|max:100|min:3',
+            'slug' => 'required|unique:stores',
+            'details' => 'required',
+        ]);
+
+        Store::create([
+            'store_name' => $request->store_name,
+            'slug' => $request->slug,
+            'details' => $request->details,
+            'user_id' => Auth()->user()->id
+        ]);
+
+        return redirect()->back()->with('message', 'Store created successfully');
     }
 }
