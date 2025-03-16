bb81767f4c9a9b4fe57a4944733262a1f0d995cc
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-16 23:21:55 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -48,4 +48,9 @@ class Product extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
 }
