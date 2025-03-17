1196b6117c7960d647e5bec4ff6063a4c381f3b1
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-03-17 22:17:00 +0530

    Update create.blade.php

--- resources/views/seller/product/create.blade.php
+++ resources/views/seller/product/create.blade.php
@@ -1,7 +1,46 @@
 @extends('seller.layouts.layout')
 @section('seller_page_title')
-    Create Product
+    Add Product
 @endsection
 @section('seller_layout')
-    <livewire:category-subcategory/>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title mb-0">Add Category</h5>
+            </div>
+            <div class="card-body">
+                @if ($errors->any())
+                    <div class="alert alert-warning alert-dismissible fade show">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (session('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
+                <form action=" " method="POST">
+                    @csrf
+                    <label for="product_name" class="fw-bold mb-2">Give Name of Your Product</label>
+                    <input type="text" class="form-control mb-2" name="product_name" placeholder="Lenovo Idealpad 5 Pro">
+                    
+                    <label for="description" class="fw-bold mb-2">Give Name of Your Product</label>
+                    <textarea name="description" class="form-control mb-2" id="description" cols="30" rows="10"></textarea>
+                    
+                    <label for="sku" class="fw-bold mb-2">SKU</label>
+                    <input type="text" class="form-control mb-2" name="sku" placeholder="LXD3402">
+                    
+                    <livewire:category-subcategory/>
+                    <button type="submit" class="btn btn-primary w-100 mt-2">Add Category</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
